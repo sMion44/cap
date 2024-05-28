d30d04d7d39cce19592078f8c1f7eee0782fbad8
Miona Suyama <suyama-miona@mdis.co.jp>
2024-05-28 03:06:45 +0000

    Add Change

--- srv/world.js
+++ srv/world.js
@@ -1,1 +1,1 @@
-module.exports = async function() { this.on("world", async(req) => { return "Hello World!" }) }
+module.exports = async function() { this.on("world", async(req) => { return "Hello World with CICD" }) }
